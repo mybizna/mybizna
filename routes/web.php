2e48dad85512ab26030390765d00e3fa7c6ea6b6
Dedan Irungu <dedanirungu@gmail.com>
2022-03-23 22:44:44 +0300

    adding bootstrap and vue

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
