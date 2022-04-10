e993d7d54310151ff19b1a5380950b67a7a09e77
Dedan Irungu <dedanirungu@gmail.com>
2022-04-10 08:28:34 +0300

    Adding material new theme https://github.com/themeselection/materio-vuetify-vuejs-admin-template-free

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/manage', [
-    'middleware' => 'auth',
     'uses' => 'App\Http\Controllers\HomeController@manage'
 ]);
