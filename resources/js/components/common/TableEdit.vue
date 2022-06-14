4faf1c15a250437ce9e68e055762d92a000daca9
Dedan Irungu <dedanirungu@gmail.com>
2022-06-14 07:27:09 +0300

    improvement to editor buttons

--- resources/js/components/common/TableEdit.vue
+++ resources/js/components/common/TableEdit.vue
@@ -12,7 +12,7 @@
                     Save
                 </a>
 
-                <a class=" rounded-lg text-sm border border-2 border-red-400 text-red-600 py-2 px-3  text-center mr-2"
+                <a class=" rounded-lg text-sm ring ring-2 ring-inset ring-red-400 text-red-600 py-2 px-3  text-center mr-2"
                     @click="cancelUrl()">
                     <i class="fa fa-plus"></i>
                     Cancel
