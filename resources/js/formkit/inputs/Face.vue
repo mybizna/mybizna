5fc53ae90695306d8fc4cc21a2a2cc17409a1496
Dedan Irungu <dedanirungu@gmail.com>
2022-06-15 10:36:43 +0300

    Adding all unique form fields

---
+++ resources/js/formkit/inputs/Face.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+    context: Object,
+})
+
+const digits = Number(props.context.digits)
+const tmp = ref(props.context.value || '')
+
+
+</script>
+
+<template>
+  Face
+</template>
