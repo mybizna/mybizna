69d20016d05df579efe8b74f620e9b0a81dda3c5
Dedan Irungu <dedanirungu@gmail.com>
2022-06-21 15:31:42 +0300

    working on listing and dropdown menu

--- public/live/assets/isp/admin/billing/list.vue
+++ public/live/assets/isp/admin/billing/list.vue
@@ -1,11 +1,6 @@
 <template>
-    <table-list
-        title="Isp Billing"
-        :path_param="path_param"
-        :search_fields="search_fields"
-        :model="model"
-        :table_fields="table_fields"
-    ></table-list>
+    <table-list title="Isp Billing" :path_param="path_param" :search_fields="search_fields" :model="model"
+        :table_fields="table_fields"></table-list>
 </template>
 
 <script>
@@ -15,19 +10,27 @@ export default {
             "components/common/TableList.vue"
         ),
     },
-    data() {
+    data () {
+
+
+
         return {
             path_param: ["isp", "billing"],
             model: {
-                id: "",
+                title: "",
+                connection_id: "",
+                invoice_id: "",
                 description: "",
-                user_id: "",
-                payment_id: "",
-                amount: "",
-                completed: "",
-                successful: "",
+                start_date: "",
+                end_date: "",
+                is_paid: "",
             },
-            search_fields: [
+            search_fields: [{
+                    type: "text",
+                    name: "title",
+                    label: "Title",
+                    ope: "",
+                },
                 {
                     type: "text",
                     name: "description",
@@ -36,55 +39,68 @@ export default {
                 },
                 {
                     type: "text",
-                    name: "user_id",
-                    label: "User Id",
+                    name: "connection_id",
+                    label: "Connection Id",
+                    ope: "",
+                },
+
+                {
+                    type: "text",
+                    name: "invoice_id",
+                    label: "Invoice Id",
                     ope: "",
                 },
                 {
-                    type: "text",
-                    name: "completed",
-                    label: "Completed",
+                    type: "datetime",
+                    name: "start_date",
+                    label: "Start Date",
                     ope: "",
                 },
                 {
-                    type: "text",
-                    name: "successful",
-                    label: "Successful",
+                    type: "datetime",
+                    name: "end_date",
+                    label: "End Date",
                     ope: "",
                 },
-                 {
-                    type: "text",
-                    name: "limit",
-                    label: "limit",
+                {
+                    type: "switch",
+                    name: "is_paid",
+                    label: "Is Paid",
                     ope: "",
                 },
             ],
             table_fields: [
                 {
-                    text: "user_id",
-                    prop: "[user.name] (user.username) <i>[user.email]</i>",
+                    text: "title",
+                    prop: "title",
                 },
                 {
                     text: "description",
                     prop: "description",
                 },
                 {
-                    text: "payment_id",
-                    prop: "payment_id",
+                    text: "invoice_id",
+                    prop: "invoice_id",
+                },
+
+                {
+                    text: "connection_id",
+                    prop: "connection_id",
                 },
                 {
-                    text: "amount",
-                    prop: "amount",
+                    text: "start_date",
+                    prop: "start_date",
+                    is_datetime: true,
                 },
+                {
+                    text: "end_date",
+                    prop: "end_date",
+                    is_datetime: true,
+                },
+
                 {
                     text: "completed",
                     prop: "completed",
-                    align: "center",
-                    is_boolean: true,
-                },
-                {
-                    text: "successful",
-                    prop: "successful",
                     align: "center",
                     is_boolean: true,
                 },
