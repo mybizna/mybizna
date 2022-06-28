b4f18168385bbe0a427e3c6f49fa08a3e335e797
Dedan Irungu <dedanirungu@gmail.com>
2022-06-28 05:12:57 +0300

    backup

--- Modules/Isp/views/admin/package/edit.vue
+++ Modules/Isp/views/admin/package/edit.vue
@@ -1,91 +1,55 @@
 <template>
-  <table-edit
-    :path_param="path_param"
-    :form_fields="form_fields"
-    :form_groups="form_groups"
-    :model="model"
-  ></table-edit>
+    <table-edit :path_param="path_param" :model="model">
+
+        <div class="row">
+            <div class="col-md-6">
+
+                <FormKit label="Id" id="id" type="hidden" validation="required" />
+                <FormKit label="Title" id="title" type="text" validation="required" />
+                <FormKit label="Connection" id="connection_id" type="text" validation="required" />
+                <FormKit label="Invoice" id="invoice_id" type="text" validation="required" />
+                <FormKit label="Start Date" id="start_date" type="datetime" validation="required" />
+                <FormKit label="End Date" id="end_date" type="datetime" validation="required" />
+
+            </div>
+            <div class="col-md-6">
+                <FormKit label="Payment" id="payment_id" type="select" validation="required" :options="[
+                    'Monaco',
+                    'Vatican City',
+                    'Maldives',
+                    'Tuvalu',
+                ]" placeholder="Select a planet" />
+                <FormKit label="Description" id="description" type="textarea" validation="required" />
+                <FormKit label="Is Paid" id="is_paid" type="switch" validation="required" />
+
+            </div>
+        </div>
+
+
+    </table-edit>
 </template>
 
 <script>
-
 export default {
-  components: {
-    TableEdit: window.$func.fetchComponent("components/common/TableEdit.vue")
-  },
-  data() {
-    return {
-      id: null,
-      path_param: ["isp", "package"],
-      form_groups: [
-        {
-          name: "setting",
-          legend: "Setting",
-          styleClasses: "flex col-lg-6 col-sm-12 setting-group"
-        },
-        {
-          name: "description",
-          legend: "Description",
-          styleClasses: "flex col-lg-6 col-sm-12 description-group"
-        }
-      ],
-      model: {
-        id: "",
-        description: "",
-        user_id: "",
-        payment_id: "",
-        amount: "",
-        completed: "",
-        successful: ""
-      },
+    components: {
+        TableEdit: window.$func.fetchComponent("components/common/TableEdit.vue")
+    },
+    data () {
+        return {
+            id: null,
+            path_param: ["isp", "billing"],
+            model: {
+                id: "",
+                title: "",
+                connection_id: "",
+                invoice_id: "",
+                description: "",
+                start_date: "",
+                end_date: "",
+                is_paid: "",
+            },
 
-      form_fields: [
-        {
-          type: "editor",
-          name: "description",
-          group: "description",
-          prefix: '"',
-          suffix: '"'
-        },
-        {
-          type: "text",
-          name: "user_id",
-          group: "main",
-          prefix: '"',
-          suffix: '"'
-        },
-        {
-          type: "text",
-          name: "payment_id",
-          group: "main",
-          prefix: '"',
-          suffix: '"',
-          readonly: true,
-          disabled: true
-        },
-        {
-          type: "text",
-          name: "amount",
-          group: "main",
-          prefix: '"',
-          suffix: '"'
-        },
-        {
-          type: "yesno",
-          name: "completed",
-          group: "setting",
-          prefix: '"',
-          suffix: '"'
-        },
-        {
-          type: "yesno",
-          name: "successful",
-          group: "setting",
-          prefix: '"',
-          suffix: '"'
-        }
-      ],
-    };
-  }
+        };
+    }
 };
 </script>
