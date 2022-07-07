e177c902560c0a47cfcf37fdcbb4996a25a3a598
Dedan Irungu <dedanirungu@gmail.com>
2022-07-07 09:37:30 +0300

    changes to core routes

--- Modules/Core/Routes/api.php
+++ Modules/Core/Routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\AuthenticationController;
@@ -16,6 +17,7 @@ use App\Http\Controllers\AuthenticationController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
 $apicontroller = 'Modules\Core\Http\Controllers\BaseController';
 
 Route::get('discover_modules', $apicontroller . '@discoverModules');
@@ -26,43 +28,6 @@ Route::get('dashboard_data', $apicontroller . '@dashboardData');
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
 
-    $DS = DIRECTORY_SEPARATOR;
-    $modules_path = realpath(base_path()) . $DS . 'Modules';
-
-    if (is_dir($modules_path)) {
-        $dir = new \DirectoryIterator($modules_path);
-
-        foreach ($dir as $fileinfo) {
-            if (!$fileinfo->isDot() && $fileinfo->isDir()) {
-                $module_name = $fileinfo->getFilename();
-
-
-                $modules_path = realpath(base_path()) . $DS . 'Modules';
-
-                if (is_dir($modules_path)) {
-                    $dir = new \DirectoryIterator($modules_path);
-
-                    foreach ($dir as $fileinfo) {
-                        if (!$fileinfo->isDot() && $fileinfo->isDir()) {
-                            $module_name = $fileinfo->getFilename();
-
-
-
-
-
-                            $menu_file = $modules_path .  $DS . $module_name .  $DS . 'menu.php';
-                            if (file_exists($menu_file)) {
-                                include_once $menu_file;
-                            }
-                        }
-                    }
-                }
-
-
-            }
-        }
-    }
-
     $apicontroller = 'Modules\Core\Http\Controllers\BaseController';
 
     Route::get('{module}/admin/{model}', $apicontroller . '@getAllRecords');
@@ -72,6 +37,54 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::put('{module}/admin/{model}/{id}', $apicontroller . '@updateRecord');
     Route::delete('{module}/admin/{model}/{id}', $apicontroller . '@deleteRecord');
     Route::match(['get', 'post'], '{module}/admin/{model}/{function}/',  $apicontroller . '@functionCall');
+
+
+    $DS = DIRECTORY_SEPARATOR;
+    $modules_path = realpath(base_path()) . $DS . 'Modules';
+    print_r($modules_path); exit;
+
+    if (is_dir($modules_path)) {
+        $dir = new \DirectoryIterator($modules_path);
+
+        foreach ($dir as $fileinfo) {
+            if (!$fileinfo->isDot() && $fileinfo->isDir()) {
+                $module_name = $fileinfo->getFilename();
+                $camel_module_name = ucfirst(Str::camel($module_name));
+                $snake_module_name = Str::lower(Str::snake($module_name));
+
+                $entities_path = $modules_path . $DS . $module_name . $DS . 'Entities';
+
+                if (is_dir($entities_path)) {
+                    $dir = new \DirectoryIterator($entities_path);
+
+                    foreach ($dir as $fileinfo) {
+                        if (!$fileinfo->isDot() && $fileinfo->isDir()) {
+                            $entity_name = $fileinfo->getFilename();
+                            $entity_name_arr = explode('.', $entity_name);
+                            $camel_entity_name = ucfirst(Str::camel($entity_name_arr[0]));
+                            $snake_entity_name = Str::lower(Str::snake($entity_name_arr[0]));
+
+                            $controller_path = realpath(base_path().'Modules/' . $camel_module_name . '/Http/Controllers/' . $camel_entity_name . 'Controller.php');
+                            $controller = 'Modules\\' . $camel_module_name . '\Http\Controllers\\' . $camel_entity_name . 'Controller';
+                            $prefix = $snake_module_name . '/admin//' . $snake_entity_name;
+
+                            print_r($controller_path); exit;
+
+                            if (file_exists($controller_path)) {
+                                Route::get($prefix, $controller . '@getAllRecords');
+                                Route::get($prefix . '/{id}', $controller . '@getRecord');
+                                Route::get($prefix . '/recordselect', $controller . '@getRecordSelect');
+                                Route::post($prefix, $controller . '@createRecord');
+                                Route::put($prefix . '/{id}', $controller . '@updateRecord');
+                                Route::delete($prefix . '/{id}', $controller . '@deleteRecord');
+                                Route::match(['get', 'post'], $prefix . '/{function}//',  $controller . '@functionCall');
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 });
 
 //register new user
