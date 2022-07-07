7e542f2955e3f67634c53abb93694f72ac99b2cf
Dedan Irungu <dedanirungu@gmail.com>
2022-07-07 17:47:07 +0300

    regular updates

--- Modules/Account/Http/Controllers/ChartOfAccountController.php
+++ Modules/Account/Http/Controllers/ChartOfAccountController.php
@@ -4,7 +4,7 @@ namespace Modules\Account\Http\Controllers;
 
 use Modules\Base\Http\Controllers\BaseController;
 
-class ChartsOfAccountController extends BaseController
+class ChartOfAccountController extends BaseController
 {
 
 }
---
+++ Modules/Product/Http/Controllers/ProductController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Product\Http\Controllers;
+
+use Modules\Base\Http\Controllers\BaseController;
+
+class ProductController extends BaseController
+{
+
+}
