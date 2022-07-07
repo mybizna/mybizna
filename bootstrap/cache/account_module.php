c2e60f5b69dbb17b477030b7fb7f21726b8e273a
Dedan Irungu <dedanirungu@gmail.com>
2022-07-07 10:59:05 +0300

    changes to route

--- bootstrap/cache/account_module.php
+++ bootstrap/cache/account_module.php
@@ -1,11 +1,9 @@
 <?php return array (
   'providers' => 
   array (
-    0 => 'Modules\\Account\\Providers\\AccountServiceProvider',
   ),
   'eager' => 
   array (
-    0 => 'Modules\\Account\\Providers\\AccountServiceProvider',
   ),
   'deferred' => 
   array (
