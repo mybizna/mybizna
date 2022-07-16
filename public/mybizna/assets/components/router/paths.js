c9a7a4e21003c055b561f20e23fc784782710c6f
Dedan Irungu <dedanirungu@gmail.com>
2022-07-16 12:09:55 +0300

    removing unneccessary files

--- public/mybizna/assets/components/router/paths.js
+++ public/mybizna/assets/components/router/paths.js
@@ -1,190 +1,109 @@
 var paths = [
-
     {
-        path: '/:catchAll(.*)',
+        path: "/:catchAll(.*)",
         meta: {
             public: true,
         },
         redirect: {
-            path: '/404'
-        }
+            path: "/404",
+        },
     },
     {
-        path: '/',
+        path: "/",
         meta: {
             breadcrumb: true,
         },
-        name: 'Root',
-        component: () => import('@/views/dashboard/Dashboard.vue'),
+        name: "Root",
+        component: () => import("@/views/dashboard/Dashboard.vue"),
         redirect: {
-            name: 'manage.dashboard'
-        }
-    }, {
-        path: '/manage/dashboard',
+            name: "manage.dashboard",
+        },
+    },
+    {
+        path: "/manage/dashboard",
         meta: {
             breadcrumb: true,
-            middlewareAuth: true
+            middlewareAuth: true,
         },
-        name: 'manage.dashboard',
-        component: () => import('@/views/dashboard/Dashboard.vue')
-
+        name: "manage.dashboard",
+        component: () => import("@/views/dashboard/Dashboard.vue"),
     },
     {
-        path: '/dashboard',
+        path: "/dashboard",
         meta: {
             public: true,
         },
-        name: 'dashboard',
-        component: () => import('@/views/dashboard/Dashboard.vue'),
+        name: "dashboard",
+        component: () => import("@/views/dashboard/Dashboard.vue"),
     },
     {
-        path: '/404',
+        path: "/404",
         meta: {
             public: true,
         },
-        name: 'NotFound',
-        component: () => import(`@/components/pages/NotFound.vue`)
+        name: "NotFound",
+        component: () => import(`@/components/pages/NotFound.vue`),
     },
     {
-        path: '/403',
+        path: "/403",
         meta: {
             public: true,
         },
-        name: 'AccessDenied',
-        component: () => import(`@/components/pages/Deny.vue`)
+        name: "AccessDenied",
+        component: () => import(`@/components/pages/Deny.vue`),
     },
     {
-        path: '/500',
+        path: "/500",
         meta: {
             public: true,
         },
-        name: 'ServerError',
-        component: () => import(`@/components/pages/Error.vue`)
+        name: "ServerError",
+        component: () => import(`@/components/pages/Error.vue`),
     },
     {
-        path: '/login',
+        path: "/login",
         meta: {
             public: true,
         },
-        name: 'Login',
-        component: () => import(`@/components/pages/Login.vue`)
-    },
-
-    {
-        path: '/autologin',
-        meta: {
-            public: true,
-        },
-        name: 'AutoLogin',
-        component: () => import(`@/components/pages/AutoLogin.vue`)
+        name: "Login",
+        component: () => import(`@/components/pages/Login.vue`),
     },
 
     {
-        path: '/register',
+        path: "/autologin",
         meta: {
             public: true,
         },
-        name: 'Register',
-        component: () => import(`@/components/pages/Register.vue`)
-    },
-    {
-        path: '/thankyou',
-        meta: {
-            public: true,
-        },
-        name: 'ThankYou',
-        component: () => import(`@/components/pages/ThankYou.vue`)
-    },
-    {
-        path: '/forgotpassword',
-        meta: {
-            public: true,
-        },
-        name: 'ForgotPassword',
-        component: () => import(`@/components/pages/ForgotPassword.vue`)
+        name: "AutoLogin",
+        component: () => import(`@/components/pages/AutoLogin.vue`),
     },
 
     {
-        path: '/typography',
+        path: "/register",
         meta: {
             public: true,
         },
-        name: 'typography',
-        component: () => import('@/views/typography/Typography.vue'),
+        name: "Register",
+        component: () => import(`@/components/pages/Register.vue`),
     },
     {
-        path: '/icons',
+        path: "/thankyou",
         meta: {
             public: true,
         },
-        name: 'icons',
-        component: () => import('@/views/icons/Icons.vue'),
+        name: "ThankYou",
+        component: () => import(`@/components/pages/ThankYou.vue`),
     },
     {
-        path: '/cards',
+        path: "/forgotpassword",
         meta: {
             public: true,
         },
-        name: 'cards',
-        component: () => import('@/views/cards/Card.vue'),
+        name: "ForgotPassword",
+        component: () => import(`@/components/pages/ForgotPassword.vue`),
     },
-    {
-        path: '/simple-table',
-        meta: {
-            public: true,
-        },
-        name: 'simple-table',
-        component: () => import('@/views/simple-table/SimpleTable.vue'),
-    },
-    {
-        path: '/form-layouts',
-        meta: {
-            public: true,
-        },
-        name: 'form-layouts',
-        component: () => import('@/views/form-layouts/FormLayouts.vue'),
-    },
-    {
-        path: '/pages/account-settings',
-        meta: {
-            public: true,
-        },
-        name: 'pages-account-settings',
-        component: () => import('@/views/pages/account-settings/AccountSettings.vue'),
-    },
-    {
-        path: '/pages/login',
-        meta: {
-            public: true,
-            layout: 'blank',
-        },
-        name: 'pages-login',
-        component: () => import('@/views/pages/Login.vue'),
-    },
-    {
-        path: '/pages/register',
-        meta: {
-            public: true,
-            layout: 'blank',
-        },
-        name: 'pages-register',
-        component: () => import('@/views/pages/Register.vue'),
-    },
-    {
-        path: '/error-404',
-        meta: {
-            public: true,
-            layout: 'blank',
-        },
-        name: 'error-404',
-        component: () => import('@/views/Error.vue'),
-    },
-
 ];
 
-
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-
-
 
 export default paths;
