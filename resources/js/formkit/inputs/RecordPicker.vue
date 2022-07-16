1bbbed76e3597624115ca440fa7dedff0fafe004
Dedan Irungu <dedanirungu@gmail.com>
2022-07-16 09:36:27 +0300

    working recordpicker. [regular updates]

--- resources/js/formkit/inputs/RecordPicker.vue
+++ resources/js/formkit/inputs/RecordPicker.vue
@@ -3,25 +3,27 @@
     <div class="p-2">
         <button type="button"
             class="mr-4 py-2 px-4 rounded-full border-0 text-sm font-semibold bg-blue-500 text-white hover:bg-blue-800"
-            @click="loadcomponent()" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            @click="loadcomponent()" data-bs-toggle="modal" :data-bs-target="'#' + context.id + 'Modal'">
             Select Record
         </button>
 
         {{ message }}
 
         <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" :id="context.id + 'Modal'" tabindex="-1" :aria-labelledby="context.id + 'ModalLabel'"
+            aria-hidden="true">
             <div class="modal-dialog ">
                 <div class="modal-content shadow-2xl shadow-indigo-500/50">
                     <div class="modal-header p-2">
-                        <h5 class="modal-title font-semibold" id="exampleModalLabel">Modal title</h5>
+                        <h5 class="modal-title font-semibold" :id="context.id + 'ModalLabel'">Modal title</h5>
                         <button @click="modalToggle" type="button" class="" data-bs-dismiss="modal" aria-label="Close">
                             <i class="fa-solid fa-circle-xmark text-2xl	text-red"></i>
                         </button>
                     </div>
 
                     <div class="modal-body p-0">
-                        <component :is="currentComp" :recordPicker="recordPicker" :is_recordpicker="is_recordpicker"></component>
+                        <component :is="currentComp" :recordPicker="recordPicker" :is_recordpicker="is_recordpicker">
+                        </component>
                     </div>
                 </div>
             </div>
@@ -33,7 +35,7 @@
 
 
 <script>
-
+import { Modal } from "bootstrap";
 import Loading from "@/formkit/inputs/Loading";
 import fetchComponent from "@/utils/fetchComponent";
 
@@ -51,19 +53,15 @@ export default {
         }
     },
     mounted () {
-        const myModalEl = document.getElementById('myModal')
+        const myModalEl = document.getElementById(this.context.id + 'Modal');
+
         myModalEl.addEventListener('hidden.bs.modal', event => {
-            // do something...
-            alert('sfdsfdsfds');
             this.modalToggle();
         });
-
-
     },
 
     methods: {
         async loadcomponent () {
-            console.log(this.context.attrs.comp_url);
             this.currentComp = await fetchComponent(
                 this.context.attrs.comp_url
             )
@@ -75,29 +73,42 @@ export default {
             window.$store.commit("system/is_recordpicker", false);
         },
         recordPicker (id) {
-            this.context.node.input(id);
-
             this.modalToggle();
+
+            //Modal(document.getElementById(this.context.id + 'Modal')).hide();
+            Modal.getOrCreateInstance(document.getElementById(this.context.id + 'Modal')).hide()
+
+            this.loadRecord(id);
         },
-        async loadRecord (id) {
-            var part1 = this.context.attrs.setting.path_param[0];
-            var part2 = this.context.attrs.setting.path_param[1];
+        loadRecord (id) {
 
-            if (id) {
+            const getdata = async (t, id) => {
+                alert('getdata');
 
+                t.message = t.context.attrs.setting.template;
+
+                var part1 = t.context.attrs.setting.path_param[0];
+                var part2 = t.context.attrs.setting.path_param[1];
                 var comp_url = part1 + "/admin/" + part2 + "/" + id;
 
-                await window.axios.get(comp_url, { params: { f: this.context.attrs.setting.fields } })
+                await window.axios.get(comp_url, { params: { f: t.context.attrs.setting.fields } })
                     .then(
                         response => {
-                            console.log(response);
-                            this.record = response.data.record;
 
-                            this.message = this.context.attrs.setting.template;
-                            var fields = this.context.attrs.setting.fields;
+                            console.log('response');
+                            console.log('');
+                            console.log('');
+                            console.log('');
+                            console.log('');
+                            console.log(response);
+
+                            t.record = response.data.record;
+
+
+                            var fields = t.context.attrs.setting.fields;
 
                             fields.forEach(item => {
-                                this.message.replaceAll('[' + item + ']', this.record[item]);
+                                t.message.replaceAll('[' + item + ']', t.record[item]);
                             })
 
 
@@ -109,7 +120,10 @@ export default {
                                 console.log('Issues Fetching Data.');
                             }
                         });
+            };
 
+            if (id) {
+                getdata(this, id);
                 this.context.node.input(id);
             }
 
