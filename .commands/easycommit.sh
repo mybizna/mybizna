01b70681f732c87705e8565f5819415b56fd6548
dedanirungu <dedanirungu@gmail.com>
2022-07-19 15:44:31 +0300

    Regular Update

--- .commands/easycommit.sh
+++ .commands/easycommit.sh
@@ -4,6 +4,7 @@ cd ..
 su - www-data
 #git config --local user.email "dedanirungu@gmail.com"
 #git config --local user.name "dedanirungu"
+#sudo git config --system --add safe.directory /var/www/html/php/laravel/laravelerp
 
 git add .
 git commit --allow-empty -m "Regular Update"
