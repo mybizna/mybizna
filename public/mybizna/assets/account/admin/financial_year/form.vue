93e3a47c04caef3ba2c1e6929bf60c1691e540e3
dedanirungu <dedanirungu@gmail.com>
2022-07-22 17:10:50 +0300

    changes to remove unecessary

---
+++ public/mybizna/assets/account/admin/financial_year/form.vue
@@ -0,0 +1,43 @@
+<template>
+    <table-edit :path_param="path_param" :model="model">
+
+        <div class="row">
+            <div class="col-md-6">
+
+                <FormKit label="Id" id="id" type="hidden" validation="required" />
+                <FormKit label="Name" id="name" type="text" validation="required" />
+                <FormKit label="Start Date" id="start_date" type="datetime" validation="required" />
+                <FormKit label="End Date" id="end_date" type="datetime" validation="required" />
+
+            </div>
+            <div class="col-md-6">
+                <FormKit label="Description" id="description" type="textarea" validation="required" />
+
+            </div>
+        </div>
+
+
+    </table-edit>
+</template>
+
+<script>
+export default {
+    components: {
+        TableEdit: window.$func.fetchComponent("components/common/TableEdit.vue")
+    },
+    data () {
+        return {
+            id: null,
+            path_param: ["account", "financial_year"],
+            model: {
+                id: "",
+                name: "",
+                description: "",
+                start_date: "",
+                end_date: "",
+            },
+
+        };
+    }
+};
+</script>
---
+++ public/mybizna/assets/isp/admin/billing_item/form.vue
@@ -0,0 +1,43 @@
+<template>
+    <table-edit :path_param="path_param" :model="model">
+
+        <div class="row">
+            <div class="col-md-6">
+
+                <FormKit label="Id" id="id" type="hidden" validation="required" />
+                <FormKit label="Title" id="title" type="text" validation="required" />
+                <FormKit label="Billing" id="billing_id" type="text" validation="required" />
+                <FormKit label="Amount" id="amount" type="text" validation="required" />
+
+            </div>
+            <div class="col-md-6">
+                <FormKit label="Description" id="description" type="textarea" validation="required" />
+
+            </div>
+        </div>
+
+
+    </table-edit>
+</template>
+
+<script>
+export default {
+    components: {
+        TableEdit: window.$func.fetchComponent("components/common/TableEdit.vue")
+    },
+    data () {
+        return {
+            id: null,
+            path_param: ["isp", "billing_item"],
+            model: {
+                id: "",
+                title: "",
+                description: "",
+                billing_id: "",
+                amount: "",
+            },
+
+        };
+    }
+};
+</script>
