874718a11997c16c4781c01cc54a978e93f3b306
Dedan Irungu <dedanirungu@gmail.com>
2022-10-31 10:14:24 +0300

    changes to setting

--- bootstrap/cache/mpesa_module.php
+++ bootstrap/cache/mpesa_module.php
@@ -1,9 +1,11 @@
 <?php return array (
   'providers' => 
   array (
+    0 => 'Modules\\Mpesa\\Providers\\MpesaServiceProvider',
   ),
   'eager' => 
   array (
+    0 => 'Modules\\Mpesa\\Providers\\MpesaServiceProvider',
   ),
   'deferred' => 
   array (
