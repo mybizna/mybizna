b3ff732a4ae0888799bf27f8107e61e86d44df67
Dedan Irungu <dedanirungu@gmail.com>
2022-11-09 10:58:26 +0300

    changes to remove env

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\AuthenticationController;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
