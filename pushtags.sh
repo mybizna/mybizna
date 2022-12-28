f02422bec4662266a0ac681a8eb58e084b730797
Dedan Irungu <dedanirungu@gmail.com>
2022-12-28 11:15:45 +0300

    Update

--- pushtags.sh
+++ pushtags.sh
@@ -2,7 +2,7 @@
 #   mybizna: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@
 #sudo chmod +x pushtags.sh && sudo ./pushtags.sh
 
-VERSION=0.9.7
+VERSION=0.9.7.2
 FOLDER=$(pwd)
 OLDVERSION=`cat version`
 
@@ -20,22 +20,30 @@ if [ $VERSION != $OLDVERSION ]
 
     echo "
 
-git tag $VERSION
+
 git submodule foreach git tag $VERSION
 
 git submodule foreach git add .
 git submodule foreach git commit --allow-empty -m 'Update'
 git submodule foreach git push origin main
 
-git push --tags
 git submodule foreach git push --tags
 
-gh release create $VERSION --generate-notes
-
 git submodule foreach gh release create $VERSION --generate-notes
+
+
+git tag $VERSION
+git add .
+git commit --allow-empty -m 'Update'
+git push origin main
+
+git push --tags
+
+gh release create $VERSION --generate-notes
     "
     #git release $VERSION 
     #git submodule foreach git release $VERSION  
+    #git submodule foreach gh release create 0.9.7 --generate-notes
 else
     echo "
 git submodule foreach git add .
