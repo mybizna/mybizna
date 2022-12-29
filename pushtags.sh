eed4f19974fe5498471e044f7ca8987ff9706f1b
Dedan Irungu <dedanirungu@gmail.com>
2022-12-29 14:27:08 +0300

    Update

--- pushtags.sh
+++ pushtags.sh
@@ -1,56 +1,79 @@
 #!/bin/sh
 # mybizna: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@
-# sudo chmod +x pushtags.sh && sudo ./pushtags.sh
+# chmod +x pushtags.sh && ./pushtags.sh
 
-VERSION=0.9.7.4
+VERSION=0.9.7.5
 FOLDER=$(pwd)
 OLDVERSION=`cat version`
 
 echo "
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 COPY COMMANDS BELOW
-
-
 "
+update_assets () {
+    rm -r ../assets/src/mybizna/css
+    rm -r ../assets/src/mybizna/fonts
+    rm -r ../assets/src/mybizna/images
+    rm -r ../assets/src/mybizna/js
+    rm -r ../assets/src/mybizna/tinymce
+
+    cp -r public/mybizna/css ../assets/src/mybizna/css
+    cp -r public/mybizna/fonts ../assets/src/mybizna/fonts
+    cp -r public/mybizna/images ../assets/src/mybizna/images
+    cp -r public/mybizna/js ../assets/src/mybizna/js
+    cp -r public/mybizna/tinymce ../assets/src/mybizna/tinymce
+}
+
+commit_assets () {
+    cd ../assets
+
+    sed -i 's/$OLDVERSION/$VERSION/g'  composer.json
+
+    git add .
+    git commit --allow-empty -m 'Update'
+    git push origin main
+    git tag $VERSION
+    git push --tags
+
+    cd ../erp
+}
+
+commit_erp_versioned () {
+    sed -i 's/$OLDVERSION/$VERSION/g'  composer.json
+    sed -i 's/$OLDVERSION/$VERSION/g'  Modules/*/composer.json
+
+    git submodule foreach git add .
+    git submodule foreach git commit --allow-empty -m 'Update'
+    git submodule foreach git push origin main
+
+    git submodule foreach git tag $VERSION
+    git submodule foreach git push --tags
+
+    git add .
+    git commit --allow-empty -m 'Update'
+    git push origin main
+
+    git tag $VERSION
+    git push --tags
+
+    git submodule foreach gh release create $VERSION --generate-notes
+    gh release create $VERSION --generate-notes
+}
+commit_erp () {
+    git submodule foreach git add .
+    git submodule foreach  git commit --allow-empty -m 'Update'
+    git submodule foreach git push origin main
+}
+
 if [ $VERSION != $OLDVERSION ]
  then
-
-    echo "sed -i 's/$OLDVERSION/$VERSION/g'  composer.json"
-    echo "sed -i 's/$OLDVERSION/$VERSION/g'  Modules/*/composer.json"
-
-    echo "
-
-
-
-git submodule foreach git add .
-git submodule foreach git commit --allow-empty -m 'Update'
-git submodule foreach git push origin main
-
-git submodule foreach git tag $VERSION
-git submodule foreach git push --tags
-
-git add .
-git commit --allow-empty -m 'Update'
-git push origin main
-
-git tag $VERSION
-git push --tags
-
-git submodule foreach gh release create $VERSION --generate-notes
-gh release create $VERSION --generate-notes
-    "
-    #git release $VERSION 
-    #git submodule foreach git release $VERSION  
-    #git submodule foreach gh release create 0.9.7 --generate-notes
+    update_assets
+    commit_assets
+    commit_erp_versioned
 else
-    echo "
-git submodule foreach git add .
-git submodule foreach  git commit --allow-empty -m 'Update'
-git submodule foreach git push origin main
-    "
+    commit_erp
 fi
 
-echo " 
-echo '$VERSION' > 'version' 
 
-"
+echo "$VERSION" > 'version' 
+
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,4 @@ Route::get('/', function () {
 });
 Auth::routes();
 
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/manage', [
-    'uses' => 'App\Http\Controllers\HomeController@manage'
-]);
