d05fa3fc7827773d786d1ee89d4d541734898ccc
Dedan Irungu <dedanirungu@gmail.com>
2023-01-15 00:58:33 +0300

    changes to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,12 +17,12 @@
 
                         @csrf
                         <div>
-                            <label for="email"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Email Address') }}</label>
-                            <input type="text" name="email" id="email" value="{{ old('email') }}"
+                            <label for="username"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Username or Email Address') }}</label>
+                            <input type="text" name="username" id="username" value="{{ old('username') }}"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('email') is-invalid @enderror"
-                                placeholder="name@company.com" required autocomplete="email" autofocus>
-                            @error('email')
+                                placeholder="name@company.com or username" required  autofocus>
+                            @error('username')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
