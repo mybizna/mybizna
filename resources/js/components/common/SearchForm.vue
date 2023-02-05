102eb5cc268b4a43db8b80c52f2b48b17532d848
Dedan Irungu <dedanirungu@gmail.com>
2023-02-05 18:47:43 +0300

    Change to Edit, List and Search vue.

--- resources/js/components/common/SearchForm.vue
+++ resources/js/components/common/SearchForm.vue
@@ -4,8 +4,9 @@
         <div class="pt-1 pr-1">
 
             <div class="input-group input-group-sm border rounded">
-                <input type="text" class="form-control dropdown-toggle border-none focus:shadow-none" placeholder="Search Any Term."
-                    aria-label="Text input with dropdown button" data-bs-toggle="dropdown" aria-expanded="false" />
+                <input type="text" class="form-control dropdown-toggle border-none focus:shadow-none"
+                    placeholder="Search Any Term." aria-label="Text input with dropdown button"
+                    data-bs-toggle="dropdown" aria-expanded="false" />
                 <div class="dropdown-menu dropdown-menu-end search-dropdown p-2 shadow-lg">
                     <b>Search</b>
                     <div class="row">
@@ -13,6 +14,11 @@
                         .system.search" :key="index" class="col-sm-6 col-md-4 col-lg-2">
                             <FormKit :label="item.label" :id="item.name" :type="item.type" validation="required" />
                         </div>
+
+                        <template v-if="show_search">
+                            <component :is="compSearch"></component>
+                        </template>
+
                         <div class="col-sm-6 col-md-4 col-lg-2">
                             <b> &nbsp; </b>
                             <div class="text-center">
@@ -85,13 +91,27 @@
             </div>
         </div>
         -->
+
     </template>
 
 </template>
 
 <script>
+
 export default {
     setup() {
+        var search_path = window.$router.currentRoute.meta.search_path;
+
+        if (search_path != '') {
+            this.compSearch = window.$func.fetchComponent(search_path);
+            this.show_search = true;
+        }
+    },
+    data() {
+        return {
+            compSearch: '',
+            show_search: false
+        }
     }
 };
 </script>
