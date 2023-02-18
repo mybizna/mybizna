92aceaaafd8cb767d716932ad5eb667c40b691ff
Dedan Irungu <dedanirungu@gmail.com>
2023-02-18 16:48:31 +0300

    Improvement on Migration Provider

--- bootstrap/cache/mpesa_module.php
+++ bootstrap/cache/mpesa_module.php
@@ -1,11 +1,9 @@
 <?php return array (
   'providers' => 
   array (
-    0 => 'Modules\\Mpesa\\Providers\\MpesaServiceProvider',
   ),
   'eager' => 
   array (
-    0 => 'Modules\\Mpesa\\Providers\\MpesaServiceProvider',
   ),
   'deferred' => 
   array (
--- pushtags.sh
+++ pushtags.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # chmod +x pushtags.sh && ./pushtags.sh
 
-VERSION=1.2.74
+VERSION=1.2.75
 FOLDER=$(pwd)
 OLDVERSION=`cat version`
 
