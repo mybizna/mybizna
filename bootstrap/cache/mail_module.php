5a27f0fdd3687341ada438e0c7ab313fb925a26a
Dedan Irungu <dedanirungu@gmail.com>
2023-02-26 20:30:32 +0300

    global notification class

--- bootstrap/cache/mail_module.php
+++ bootstrap/cache/mail_module.php
@@ -1,9 +1,11 @@
 <?php return array (
   'providers' => 
   array (
+    0 => 'Modules\\Mail\\Providers\\EventServiceProvider',
   ),
   'eager' => 
   array (
+    0 => 'Modules\\Mail\\Providers\\EventServiceProvider',
   ),
   'deferred' => 
   array (
