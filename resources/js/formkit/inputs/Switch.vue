f886008eb48e5d555efe41362d95f281924a4ef8
Dedan Irungu <dedanirungu@gmail.com>
2023-03-01 19:28:49 +0300

    working of form switch

--- resources/js/formkit/inputs/Switch.vue
+++ resources/js/formkit/inputs/Switch.vue
@@ -1,13 +1,19 @@
 <template>
-    <div class="flex justify-start">
+    <div class="flex justify-start" @click="selection">
         <div class="form-check form-switch">
-            <input
-                class="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-white bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm"
-                 v-model="selected" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+
+            <label for="toggleTwo" class="flex cursor-pointer select-none items-center">
+                <div class="relative">
+                    <input type="checkbox" id="toggleTwo" class="sr-only" />
+                    <div class="block h-6 w-10 rounded-full bg-[#E5E7EB]"></div>
+                    <div :class="selected ? 'bg-blue-800 right-1 ' : 'bg-white left-1 '"
+                        class="dot absolute top-1 h-4 w-4 rounded-full  transition"></div>
+                </div>
+            </label>
+
         </div>
     </div>
 </template>
-
 
 <script>
 
@@ -16,15 +22,30 @@ export default {
         context: Object,
     },
     watch: {
+        'context.value': function (newVal, oldVal) {
+            if (this.selected != newVal) {
+                this.selected = newVal;
+            }
+        },
         selected: function (newVal, oldVal) {
-            this.context.node.input(newVal);
+            if (newVal !== oldVal) {
+                this.context.node.input(newVal);
+            }
         },
 
-    }, data () {
+    },
+    data() {
         return {
             selected: '',
         }
     },
+    methods: {
+        selection(e) {
+            e.preventDefault();
+
+            this.selected = !this.selected;
+        }
+    }
 }
 </script>
 
