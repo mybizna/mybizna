4807a81cabc6c9eff556cb18a2829439025737bc
Dedan Irungu <dedanirungu@gmail.com>
2023-03-03 09:35:21 +0300

    sms reader

---
+++ bootstrap/cache/smsreader_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Smsreader\\Providers\\EventServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Smsreader\\Providers\\EventServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
--- pushtags.sh
+++ pushtags.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # chmod +x pushtags.sh && ./pushtags.sh
 
-VERSION=1.3.6
+VERSION=1.3.7
 FOLDER=$(pwd)
 OLDVERSION=`cat version`
 
