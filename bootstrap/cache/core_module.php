dd79f41a27390cc12246b6d25013c395bcdf0695
Dedan Irungu <dedanirungu@gmail.com>
2023-05-09 12:19:06 +0300

    improvement to language

--- bootstrap/cache/core_module.php
+++ bootstrap/cache/core_module.php
@@ -1,9 +1,11 @@
 <?php return array (
   'providers' => 
   array (
+    0 => 'Modules\\Core\\Providers\\EventServiceProvider',
   ),
   'eager' => 
   array (
+    0 => 'Modules\\Core\\Providers\\EventServiceProvider',
   ),
   'deferred' => 
   array (
