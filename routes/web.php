20fa7e125dc43fb98ce80007adcc362842f3eae3
Dedan Irungu <dedanirungu@gmail.com>
2023-08-27 10:28:41 +0300

    changes to router

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+            
+            
+
+use Illuminate\Support\Facades\Route;
+
+use Modules\Base\Http\Controllers\BaseController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [BaseController::class, 'manage']);
+
+
