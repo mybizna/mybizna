d201923ac9a3960ff6d5f968936b9fa1023de8d4
Dedan <dedanirungu@gmail.com>
2023-09-28 15:34:52 +0300

    Changes to files: public/mybizna/js/519.js public/mybizna/js/519.js.map public/mybizna/js/app.js resources/js/components/pages/Frontend.vue

--- resources/js/components/pages/Frontend.vue
+++ resources/js/components/pages/Frontend.vue
@@ -23,46 +23,11 @@
         </div>
         <div class="w-full lg:w-3/5 px-3 mb-12 lg:mb-0">
           <div class="lg:h-128 flex items-center justify-center"><img class="lg:max-w-lg"
-              src="/assets/imgs/illustrations/work-tv.png" alt="Monst"></div>
+              src="/images/work-tv.png" alt="Monst"></div>
         </div>
       </div>
     </div>
   </section>
-
-  <section class="py-12 md:pt-20 md:pb-12" id="how-we-work">
-    <div class="container">
-      <div class="max-w-lg mx-auto mb-20 text-center"><span
-          class="inline-block text-xs py-1 px-3 text-blue-700 font-semibold bg-blue-100 rounded-xl wow animate__animated animate__fadeIn"
-          data-wow-delay=".5s">How we work</span>
-        <h2
-          class="my-3 text-3xl md:text-4xl text-blueGray-900 font-bold font-heading wow animate__animated animate__fadeIn"
-          data-wow-delay="0">Partner Company Business And Me Success Fully</h2>
-        <p class="text-blueGray-400 wow animate__animated animate__fadeIn" data-wow-delay=".3s">IT Solution For Your
-          Business Is Best Theme</p>
-      </div>
-      <div class="relative max-w-6xl mx-auto wow animate__animated animate__fadeIn" data-wow-delay=".3s"><img
-          src="/assets/imgs/elements/macbook.png" alt="Monst">
-        <div class="absolute" style="top: 5.8%; left: 14.6%; width: 72.8%; height: 76.7%;">
-          <div class="relative w-full">
-            <div class="carausel-fade-2 slick-carausel dot-style-1" id="carausel-fade-2-1">
-              <div
-                class="swiper swiper-initialized swiper-horizontal swiper-pointer-events custom-class swiper-backface-hidden">
-                <div class="swiper-wrapper" style="transform: translate3d(0px, 0px, 0px);">
-                  <div class="swiper-slide swiper-slide-active" style="width: 839px; margin-right: 30px;"><img
-                      class="rounded" src="/assets/imgs/placeholders/mockup-1.png" alt="Monst"></div>
-                  <div class="swiper-slide swiper-slide-next" style="width: 839px; margin-right: 30px;"><img
-                      class="rounded" src="/assets/imgs/placeholders/mockup-2.png" alt="Monst"></div>
-                  <div class="swiper-slide" style="width: 839px; margin-right: 30px;"><img class="rounded"
-                      src="/assets/imgs/placeholders/mockup-3.png" alt="Monst"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
 
   <section class="py-20 xl:bg-contain bg-top bg-no-repeat" id="key-features"
     style="background-image: url(&quot;assets/imgs/backgrounds/intersect.svg&quot;);">
