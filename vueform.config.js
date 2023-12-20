08a7085048324d98e48192974fa6cd92d4f49a1b
Dedan <dedanirungu@gmail.com>
2023-12-20 03:59:26 +0300

    Changes to files: public/mybizna/js/app.js public/mybizna/js/app.js.map vueform.config.js

--- vueform.config.js
+++ vueform.config.js
@@ -15,7 +15,9 @@ import YearpickerElement from '@/vueform/YearpickerElement.vue'
 // You might place these anywhere else in your project
 import '@vueform/vueform/themes/vueform/css/index.min.css';
 
-let input = 'form-input rounded border py-2 px-3 focus:border-sky-500 hover:border-sky-500 text-grey-800 h-8 w-full';
+let input = 'form-input rounded border py-2 px-3 focus:border-sky-500 hover:border-sky-500 text-grey-800 w-full';
+let text = input + 'h-10';
+let textarea = input + 'h-50';
 
 
 export default defineConfig({
@@ -52,11 +54,31 @@ export default defineConfig({
         TextElement: {
             container: '',
             label: '',
+            wrapper:'w-full',
             inputContainer: 'w-full flex',
             inputContainer_default: 'border-black',
             inputContainer_focused: 'border-red-500',
             inputContainer_md: '',
-            input: input,
+            input: text,
+            input_enabled: 'focus:form-ring',
+            input_disabled: 'form-bg-disabled form-text-disabled',
+            input_sm: 'form-p-input-sm form-rounded-sm form-text-sm',
+            input_md: 'form-p-input form-rounded',
+            input_lg: 'form-p-input-lg form-rounded-lg form-text-lg with-floating:form-p-input-floating-lg',
+            $input: (classes, { isDisabled, Size }) => ([
+                classes.input,
+                classes[`input_${Size}`],
+                isDisabled ? classes.input_disabled : classes.input_enabled
+            ]),
+        },
+        TextareaElement: {
+            container: '',
+            label: '',
+            inputContainer: 'w-full flex',
+            inputContainer_default: 'border-black',
+            inputContainer_focused: 'border-red-500',
+            inputContainer_md: '',
+            input: textarea,
             input_enabled: 'focus:form-ring',
             input_disabled: 'form-bg-disabled form-text-disabled',
             input_sm: 'form-p-input-sm form-rounded-sm form-text-sm',
