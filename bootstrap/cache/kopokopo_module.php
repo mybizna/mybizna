89af6e7398bbe5974030f2190cf536447165fd87
Dedan <dedanirungu@gmail.com>
2024-02-21 21:13:16 +0300

    Changes to files: bootstrap/cache/kopokopo_module.php resources/views/layouts/app.blade.php

--- bootstrap/cache/kopokopo_module.php
+++ bootstrap/cache/kopokopo_module.php
@@ -1,9 +1,11 @@
 <?php return array (
   'providers' => 
   array (
+    0 => 'Modules\\Kopokopo\\Providers\\EventServiceProvider',
   ),
   'eager' => 
   array (
+    0 => 'Modules\\Kopokopo\\Providers\\EventServiceProvider',
   ),
   'deferred' => 
   array (
