7c7daa9a811462ccf73b11b766bde4e4f03187de
Dedan <dedanirungu@gmail.com>
2024-03-25 19:46:00 +0300

    Add topbar icons and update sidebar layout

--- templates/default/manage/AppTemplate.vue
+++ templates/default/manage/AppTemplate.vue
@@ -1,12 +1,59 @@
 <template>
     <template v-if="$store.getters['auth/loggedIn']">
 
+        <div class="w-full z-10 shadow-lg bg-white">
+
+            <div class="flex border-b border-gray-100 py-1">
+                <div v-if="sidebar_show" class="flex-none w-48 "> </div>
+
+                <div class="flex-none w-20">
+                    <div class="flex justify-start pt-1 pl-1 mr-2 space-x-4 cursor-pointer text-blue-500">
+
+                        <a @click="drawer" class="w-15">
+                            <i :class="$store.state.system.applist ? 'text-cyan-200' : ' text-blue-500'"
+                                class="fab fa-microsoft text-2xl "></i>
+
+                            <div :class="$store.state.system.applist ? 'text-cyan-200' : 'text-blue-500'"
+                                class="inline-block text-sm font-bold  pl-1">
+                                APP
+                            </div>
+
+
+                            <span
+                                class="absolute inline-flex items-center justify-center w-4 h-4 leading-3 font-bold text-white bg-blue-500 border border-white rounded-full top dark:border-gray-900 "
+                                style="margin-left:-10px; font-size: 9px !important;">
+                                {{ $store.state.system.backendmenu_length - 1 }}
+                            </span>
+                        </a>
+
+                    </div>
+                </div>
+
+                <div class="flex-auto text-right">
+                    <div class="flex justify-end pt-1 pr-1 pb-1 mr-2 space-x-2 cursor-pointer">
+                        <app-topbar-icon-others></app-topbar-icon-others>
+                        <app-topbar-icon-avatar></app-topbar-icon-avatar>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <app-topbar :windowWidth="windowWidth"></app-topbar>
 
         <div class="overflow-x-hidden">
             <div class="flex">
-                <div class="flex-none">
-                    <app-sidebar></app-sidebar>
+                <div v-if="sidebar_show" class="flex-none w-48 ">
+                    <template v-for="(item, m_index) in $store.state.system.backendmenu" :key="m_index">
+                        <div class="inline-block cursor-pointer w-28 my-3 mb-3 mx-2 sm:mx-5 sm:mx-7"
+                            @click="selectApp(m_index)">
+
+                            <div class="mt-1">
+                                {{ item.title }}
+                                <span v-if="countObjectKeys(item.menus)" class="text-xs"> &#10148; </span>
+                            </div>
+
+                        </div>
+                    </template>
                 </div>
                 <div class="flex-auto">
 
@@ -47,8 +94,9 @@
 export default {
     components: {
         AppSidebar: window.$filters.fetchComponent('templates/default/manage/AppSidebar.vue'),
-        AppTopbar: window.$filters.fetchComponent('templates/default/manage/AppTopbar.vue'),
         AppTopbarActions: window.$filters.fetchComponent('templates/default/manage/AppTopbarActions.vue'),
+        AppTopbarIconAvatar: window.$filters.fetchComponent('templates/default/manage/AppTopbarIconAvatar.vue'),
+        AppTopbarIconOthers: window.$filters.fetchComponent('templates/default/manage/AppTopbarIconOthers.vue'),
     },
     props: {
         windowWidth: { type: Number, default: window.innerWidth },
@@ -56,8 +104,22 @@ export default {
     },
     data() {
         return {
+            sidebar_show: true,
             currentYear: new Date().getFullYear(),
         };
     },
+    methods: {
+        countObjectKeys(obj) {
+            return Object.keys(obj).length;
+        },
+        updateSidebarShow() {
+            window.scrollTo(0, 0);
+            if (window.innerWidth < this.$responsive_point) {
+                this.$store.commit("system/sidebar_show", false);
+            }
+        },
+
+        selectApp(passkey) { }
+    }
 };
 </script>
