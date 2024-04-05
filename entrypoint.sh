e932009695489638d416d6069d4548f56c3f473d
Dedan <dedanirungu@gmail.com>
2024-04-05 19:37:41 +0300

    Update entrypoint.sh script

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,6 @@ sed -i 's/DB_HOST=.*/DB_HOST=mariadb/g' .env
 sed -i 's/DB_USERNAME=.*/DB_USERNAME=root/g' .env
 sed -i 's/DB_PORT=.*/DB_PORT=3306/g' .env 
 
-cat /var/www/html/.env
-
 rm -rf Modules/*
 rm composer.lock
 
@@ -17,11 +15,6 @@ composer install --no-interaction
 
 composer require mybizna/isp:24.3.004 --no-interaction
 
-echo '----------'
-echo '-----mariadb-----'
-
-mysql -u root -h mariadb -e "CREATE DATABASE IF NOT EXISTS mybizna;"
-mysql -u root -h mariadb -e "SHOW DATABASES;"
 
 php artisan key:generate
 
