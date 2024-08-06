f9c266b915d275448b5429230bd68c038d4c19dd
Dedan <dedanirungu@gmail.com>
2024-08-06 20:52:26 +0300

    Changes to files: Modules/Account Modules/Core Modules/Mail Modules/Partner Modules/Product Modules/Sale composer.json composer.lock public/js/filament/tables/components/table.js public/js/guava/calendar/components/calendar-widget.js pushtags.sh

--- pushtags.sh
+++ pushtags.sh
@@ -1,33 +1,7 @@
 #!/bin/sh
 # chmod +x pushtags.sh && ./pushtags.sh
 
-echo "
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-Starting Commit Process
-"
-update_assets () {
-    rm -r ../assets/src/templates/default
-    rm -r ../assets/src/mybizna/css
-    rm -r ../assets/src/mybizna/images
-    rm -r ../assets/src/mybizna/js
-    rm -r ../assets/src/mybizna/tailwind
-    rm -r ../assets/src/mybizna/tinymce
-    rm -r ../assets/src/mybizna/fontawesome
-    rm -r ../assets/src/mybizna/vue3-sfc-loader
 
-    mkdir ../assets/src/mybizna/assets
-
-    cp -r templates/default ../assets/src/templates/default
-    cp -r public/mybizna/css ../assets/src/mybizna/css
-    cp -r public/mybizna/images ../assets/src/mybizna/images
-    cp -r public/mybizna/js ../assets/src/mybizna/js
-    cp -r public/mybizna/tinymce ../assets/src/mybizna/tinymce
-    cp -r public/mybizna/tailwind ../assets/src/mybizna/tailwind
-    cp -r public/mybizna/fontawesome ../assets/src/mybizna/fontawesome
-    cp -r public/mybizna/vue3-sfc-loader ../assets/src/mybizna/vue3-sfc-loader
-
-    find "../assets/src/mybizna/js" -type f -name "*.js.map" -exec rm -f {} +
-}
 
 commit_modules () {
 
@@ -75,150 +49,7 @@ commit_modules () {
 }
 
 
-commit_assets () {
-    cd ../assets
-
-    echo ""
-    echo "-------------------"
-    echo "Committing Assets"
-    echo ""
-
-    # Check if there are any changed files
-    changed_files=$(git diff --name-only)
-    if [ -z "$changed_files" ]; then
-        echo "No changed files. Skipping..."
-    else
-        read -p "Enter commit message for assets (or press Enter for default): " commit_message
-
-        # Check if commit_message is empty
-        if [ -z "$commit_message" ]; then
-            commit_message="Changes to files: $changed_files"
-        fi
-        
-        git add .
-        git commit -m "$commit_message"
-        git push origin main
-    fi
-
-    last_release_commit=$(git describe --abbrev=0 --tags)
-    commit_count=$(git rev-list --count "$last_release_commit"..HEAD)
-
-    if [ "$commit_count" -gt 0 ]; then
-
-        # Extract the current version from the module's composer.json
-        current_version=$(jq -r '.version' composer.json)
-
-        major=$(echo "$current_version" | cut -d'.' -f1)
-        minor=$(echo "$current_version" | cut -d'.' -f2)
-        patch=$(echo "$current_version" | cut -d'.' -f3)
-
-        # Increment the patch number with a maximum value of 50
-        if [ "$patch" -lt 50 ]; then
-            patch=$(expr $patch + 1)  
-        else
-            minor=$(expr $minor + 1)  
-            patch=1
-        fi                
-
-        # Pad the patch number with three zeros
-        patch=$(printf "%03d" $patch)  
-
-        # Construct the new version
-        new_version="$major.$minor.$patch"
-
-        MESSAGE="Release $new_version"
-
-        jq ".version=\"$new_version\"" composer.json > tmp_composer.json
-        echo yes | mv tmp_composer.json composer.json
-
-        git add .
-        git commit --allow-empty -m "$MESSAGE"
-        git push origin main
-        git tag $new_version
-        git push --tags
-
-        gh repo set-default
-        gh release create $new_version --generate-notes
-
-        cd ../erp
-
-    fi
-}
-
-
-commit_migration () {
-    cd ../migration
-
-    echo ""
-    echo "-------------------"
-    echo "Committing Migration"
-    echo ""
-
-    # Check if there are any changed files
-    changed_files=$(git diff --name-only)
-    if [ -z "$changed_files" ]; then
-        echo "No changed files. Skipping..."
-    else
-        read -p "Enter commit message for migration (or press Enter for default): " commit_message
-
-        # Check if commit_message is empty
-        if [ -z "$commit_message" ]; then
-            commit_message="Changes to files: $changed_files"
-        fi
-        
-        git add .
-        git commit -m "$commit_message"
-        git push origin main       
-    fi
-    
-
-    last_release_commit=$(git describe --abbrev=0 --tags)
-    commit_count=$(git rev-list --count "$last_release_commit"..HEAD)
-
-    if [ "$commit_count" -gt 0 ]; then
-
-        # Extract the current version from the module's composer.json
-        current_version=$(jq -r '.version' composer.json)
-
-        major=$(echo "$current_version" | cut -d'.' -f1)
-        minor=$(echo "$current_version" | cut -d'.' -f2)
-        patch=$(echo "$current_version" | cut -d'.' -f3)
-
-        # Increment the patch number with a maximum value of 50
-        if [ "$patch" -lt 50 ]; then
-            patch=$(expr $patch + 1)  
-        else
-            minor=$(expr $minor + 1)  
-            patch=1
-        fi                
-
-        # Pad the patch number with three zeros
-        patch=$(printf "%03d" $patch)  
-
-
-        # Construct the new version
-        new_version="$major.$minor.$patch"
-
-        MESSAGE="Release $new_version"
-
-        jq ".version=\"$new_version\"" composer.json > tmp_composer.json
-        echo yes | mv tmp_composer.json composer.json
-
-        git add .
-        git commit --allow-empty -m "$MESSAGE"
-        git push origin main
-        git tag $new_version
-        git push --tags
-
-        gh repo set-default
-        gh release create $new_version --generate-notes
-
-    fi
-
-    cd ../erp
-}
-
-commit_module () {
+upload_modules () {
     echo ""
     echo "-------------------"
     echo "Committing Modules"
@@ -318,10 +149,6 @@ commit_erp () {
 
     if [ "$commit_count" -gt 0 ]; then
 
-        update_assets
-
-        commit_assets
-
         # Extract the current version from the module's composer.json
         current_version=$(jq -r '.version' composer.json)
 
@@ -363,8 +190,7 @@ commit_erp () {
 }
 
 commit_modules
-commit_migration
-commit_module
+upload_modules
 commit_erp
 
 
