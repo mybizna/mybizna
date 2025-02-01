c38ca058a3b7eead10821ad10a93b6141f2b3949
Dedan <dedanirungu@gmail.com>
2025-02-01 12:44:08 +0300

    Changes to files: app/Models/User.php app/Providers/Filament/AdminPanelProvider.php public/js/filament/notifications/notifications.js

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,10 +1,6 @@
 <?php
 namespace App\Models;
 
-<<<<<<< HEAD
-=======
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
->>>>>>> 8946dc6df92e05df38f1730fd54eb6665ede2343
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -3,10 +3,7 @@
 namespace App\Providers\Filament;
 
 use Filament\Http\Middleware\Authenticate;
-<<<<<<< HEAD
 use Filament\Http\Middleware\AuthenticateSession;
-=======
->>>>>>> 8946dc6df92e05df38f1730fd54eb6665ede2343
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
@@ -18,17 +15,8 @@ use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
-<<<<<<< HEAD
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-=======
-use Illuminate\Session\Middleware\AuthenticateSession;
-use Illuminate\Session\Middleware\StartSession;
-use Illuminate\View\Middleware\ShareErrorsFromSession;
-use Coolsam\Modules\ModulesPlugin;
-
-// use UnexpectedJourney\FilamentResourcePicker\FilamentResourcePickerPlugin;
->>>>>>> 8946dc6df92e05df38f1730fd54eb6665ede2343
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -42,20 +30,11 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
-<<<<<<< HEAD
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 Pages\Dashboard::class,
             ])
-=======
-            ->sidebarWidth('16rem')
-            ->sidebarFullyCollapsibleOnDesktop()
-            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
-            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
-            ->discoverClusters(in: app_path('Filament/Clusters'), for: 'App\\Filament\\Clusters')
-            ->pages([ Pages\Dashboard::class, ])
->>>>>>> 8946dc6df92e05df38f1730fd54eb6665ede2343
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
@@ -74,18 +53,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-<<<<<<< HEAD
             ]);
     }
 }
-=======
-            ])
-            ->plugins([ModulesPlugin::make(),])
-            // ->plugins([ModulesPlugin::make(), FilamentResourcePickerPlugin::make(),])
-            ->viteTheme('resources/css/filament/admin/theme.css')
-            ;
-            print('panel Hello World'); exit;
-    }
-}
-
->>>>>>> 8946dc6df92e05df38f1730fd54eb6665ede2343
